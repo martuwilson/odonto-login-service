0e1d3c98e6374e9f3f94a8066274b9a7a3401bb4
Martin Ezequiel Williner <williner.dev@gmail.com>
2025-01-12 19:39:14 -0300

    feat: agregar ruta  y configurar rutas de usuarios

--- src/index.js
+++ src/index.js
@@ -4,6 +4,16 @@ import pool from './database/index.js';
 const app = express();
 dotenv.config();
 app.use(express.json());
+
+app.use(express.json());
+app.get('/', (req, res) => {
+    res.send('Bienvenido a la API de gestión de stock de materiales!');
+  });
+  
+
+app.use('/api/users', usersRoutes);
+
+
 app.listen(process.env.PORT, () => {
     console.log(`Server listening on port ${process.env.PORT}`);
 });
